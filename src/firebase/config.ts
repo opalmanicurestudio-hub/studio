81caebf73a26a9b1aec8c87c1c2c23870f06b69b
Opal <opalmanicurestudio@gmail.com>
2026-01-08 02:02:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2009819169-8322c",
+  "appId": "1:455110761282:web:fb5ab0b71bd7a945f346db",
+  "apiKey": "AIzaSyAlwP6wvJPOvbMV7ZUoj0gjWwyjpnyaq24",
+  "authDomain": "studio-2009819169-8322c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "455110761282"
+};
