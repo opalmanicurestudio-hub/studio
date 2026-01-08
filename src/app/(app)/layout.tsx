035ea2c8aaf1f07b78206fed9fcc277d64ca1484
Opal <opalmanicurestudio@gmail.com>
2026-01-08 02:03:35 +0000

    File changes

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,11 +1,14 @@
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/shared/AppSidebar';
+import { FirebaseClientProvider } from '@/firebase';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>{children}</SidebarInset>
-    </SidebarProvider>
+    <FirebaseClientProvider>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>{children}</SidebarInset>
+      </SidebarProvider>
+    </FirebaseClientProvider>
   );
 }
