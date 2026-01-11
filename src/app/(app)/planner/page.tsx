935371911cf0925781a381da62bf78acb34cb24a
Opal <opalmanicurestudio@gmail.com>
2026-01-11 17:04:53 +0000

    It’s currently not working on desktop still do you have any reason as to

--- src/app/(app)/planner/page.tsx
+++ src/app/(app)/planner/page.tsx
@@ -269,7 +269,7 @@ const DayTimeline = ({
                 )}
             </div>
 
-            <ScrollArea className="flex-1" style={{ height: 'calc(100vh - 230px)' }}>
+            <ScrollArea className="flex-1">
                 <div className="grid grid-cols-[auto,1fr] p-4">
                     {/* Time labels */}
                     <div className="flex flex-col text-right pr-4">
@@ -626,7 +626,7 @@ export default function PlannerPage() {
         ))}
       </div>
 
-      <main className="flex-1 overflow-hidden">
+      <main className="flex-1 min-h-0">
          <Carousel setApi={setApi} className="h-full w-full" opts={{startIndex: currentDayIndex, align: 'start' }}>
             <CarouselContent className="h-full">
                  {weekDays.map((date, index) => {
@@ -721,4 +721,5 @@ export default function PlannerPage() {
     </div>
   );
 }
+
 
