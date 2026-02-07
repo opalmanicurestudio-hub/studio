1d5f91f2fa4917f41f5149dce34264bb7bd8ac9a
Opal <opalmanicurestudio@gmail.com>
2026-02-07 00:31:36 +0000

    Try fixing this error: `Build Error: You cannot have two parallel pages

--- src/app/(app)/walk-in/[tenantId]/page.tsx
+++
@@ -1,6 +0,0 @@
-
-// This page is intentionally left blank to resolve a routing conflict.
-// The correct public kiosk page can be found at /kiosk/[tenantId].
-export default function ConflictingWalkInPage() {
-  return null;
-}
