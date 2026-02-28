e3be1d118812b31a1e502a5c3169ba4d4f83778f
Opal <opalmanicurestudio@gmail.com>
2026-02-28 20:46:42 +0000

    Try fixing this error: `Runtime ReferenceError: Can't find variable: Dia

--- src/app/(app)/staff/page.tsx
+++ src/app/(app)/staff/page.tsx
@@ -46,6 +46,14 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from '@/components/ui/dialog';
 import Link from 'next/link';
 import { useTenant } from '@/context/TenantContext';
 import { formatPhoneNumber } from 'react-phone-number-input';
