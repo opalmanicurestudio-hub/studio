599f67d0f86d852c3340d7179097768249a45ae0
Opal <opalmanicurestudio@gmail.com>
2026-03-06 18:56:27 +0000

    Try fixing this error: `Runtime ReferenceError: Can't find variable: Dol

--- src/components/pos/WalkInQueue.tsx
+++ src/components/pos/WalkInQueue.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import React, { useState, useMemo } from 'react';
@@ -7,7 +6,7 @@ import { WaitingCustomerCard } from './WaitingCustomerCard';
 import { type WalkIn, type Staff, type Service, type Appointment, type Client } from '@/lib/data';
 import { AssignStaffDialog } from './AssignStaffDialog';
 import { Button } from '../ui/button';
-import { Sparkles, TrendingUp, Users, Clock, CheckCircle, Activity, QrCode, Play, ShoppingCart } from 'lucide-react';
+import { Sparkles, TrendingUp, Users, Clock, CheckCircle, Activity, QrCode, Play, ShoppingCart, DollarSign } from 'lucide-react';
 import { ScrollArea } from '../ui/scroll-area';
 import { cn } from '@/lib/utils';
 import { InServiceAppointmentCard } from './InServiceAppointmentCard';
