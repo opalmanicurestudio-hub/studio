72d5316a3afb5de30b7ca13a1e6766f95b7cdab1
opalmanicurestudio-hub <opalmanicurestudio@gmail.com>
2026-05-03 21:02:04 -0400

    Update AddRefreshmentDialog.tsx

--- src/components/inventory/AddRefreshmentDialog.tsx
+++ src/components/inventory/AddRefreshmentDialog.tsx
@@ -212,7 +212,7 @@ export function AddRefreshmentDialog(props: Props) {
       </div>
 
       {/* Scrollable body */}
-      <ScrollArea className="flex-1 min-h-0">
+      <div className="flex-1 min-h-0 overflow-y-auto overscroll-contain">
         <AnimatePresence mode="wait">
 
           {step === 1 && (
