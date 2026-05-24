05ab883d0c4c9c0c255441e5a0d7e4148f71e4e5
opalmanicurestudio-hub <opalmanicurestudio@gmail.com>
2026-05-23 22:19:50 -0400

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "klair-a5718",
-  "appId": "1:383195395473:web:8d3ea3f7753eb0e7471630",
-  "apiKey": "AIzaSyAqtg7UYbq1HtLpYs5qpJzjcgDyC7-AkJc",
-  "authDomain": "klair-a5718.firebaseapp.com",
-  "measurementId": "G-Y6ZLGGFTSJ",
-  "messagingSenderId": "383195395473"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
