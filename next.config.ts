3f44f3ae01e16d83ee699bbbdc52bb99a7748048
opalmanicurestudio-hub <opalmanicurestudio@gmail.com>
2026-06-17 00:56:19 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
-
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: true,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -37,5 +37,4 @@ const nextConfig: NextConfig = {
     ],
   },
 };
-
 export default nextConfig;
