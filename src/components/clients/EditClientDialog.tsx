099d145216113998e65f9de9902d395db3a9ce66
opalmanicurestudio-hub <opalmanicurestudio@gmail.com>
2026-08-08 09:10:01 -0400

    Fix formatting and import statements in EditClientDialog

--- src/components/clients/EditClientDialog.tsx
+++ src/components/clients/EditClientDialog.tsx
@@ -1,6 +1,6 @@
-
 'use client';
 
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useState, useEffect } from 'react';
 import { useIsMobile } from '@/hooks/use-mobile';
 import {
